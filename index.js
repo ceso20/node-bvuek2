1a8bd0691de91cbb164009fc5beb8d4e76d68fdb
ceso20 <106763997+ceso20@users.noreply.github.com>
2022-11-23 22:01:03 +0300

    3 files modified

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("MUSTAFA")
+const express = require('express');
+const app = express();
+
+app.get("/", (req,res) => {
+  res.send("<h1>Mustafa</h1>")
+})
+
+
+
+
+app.listen(1234, () => {
+  console.log('started...');
+});
